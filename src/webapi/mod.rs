d18bfa323cda0b1340920f850f164692c0153da4
stnik <stnik@proton.me>
2025-05-05 20:30:00 +0200

    Set default  root url to index compiled to the binary

--- src/webapi/mod.rs
+++ src/webapi/mod.rs
@@ -5,13 +5,25 @@ mod proc;
 mod files;
 
 use std::sync::Arc;
+use actix_web::http::header;
+use actix_web::http::header::ContentType;
 use actix_web::web;
 use actix_web::web::Data;
 use actix_web::App;
+use actix_web::HttpResponse;
 use actix_web::HttpServer;
-
 use crate::util::config::Config;
 use crate::register::RegisterRef;
+
+static INDEX: &'static str = include_str!("../../resources/var/fs/app/index.html");
+
+
+pub(crate) async fn index() -> HttpResponse{
+    HttpResponse::Ok()
+    .insert_header((header::CONTENT_TYPE, ContentType::html().to_string()))
+    .body(INDEX)
+}
+
 
 pub fn create_server(config: Arc<Config>, reg: RegisterRef) -> std::io::Result<actix_web::dev::Server> {
     let json_default_limit = config.net.json_default_limit;
@@ -25,6 +37,7 @@ pub fn create_server(config: Arc<Config>, reg: RegisterRef) -> std::io::Result<a
             .app_data(web::JsonConfig::default().limit(json_default_limit)) //limit size of the payload (global configuration)
             .configure(proc::http::configure_http)
             .configure(files::configure_files)
+            .service(web::resource("/").route(web::get().to(index)))
             // .service(Files::new(
             //     "/process/{alias}/log/list", 
             //     &cfg.path.log_dir).show_files_listing())
